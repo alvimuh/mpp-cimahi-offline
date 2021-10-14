7e548610a4ce5bc288fe46d9ced0b63e4116f45c
Dion Arya Pamungkas <dionarya.p@gmail.com>
2021-10-14 07:13:18 +0700

    consume api

--- src/pages/LihatAntrian/index.js
+++ src/pages/LihatAntrian/index.js
@@ -1,51 +1,23 @@
 import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import "./style.css";
+import axios from 'axios';
 
 function LihatAntrian() {
-  const [data, setData] = useState([
-    {
-      name: "Handika",
-      urutan: "001",
-      loket: "1",
-    },
-    {
-      name: "Ayunda",
-      urutan: "003",
-      loket: "3",
-    },
-    {
-      name: "Cecep",
-      urutan: "004",
-      loket: "2",
-    },
-    {
-      name: "Danila",
-      urutan: "002",
-      loket: "4",
-    },
-    {
-      name: "Muhammad Opik",
-      urutan: "005",
-      loket: "5",
-    },
-    {
-      name: "Jayadi Saputra",
-      urutan: "006",
-      loket: "6",
-    },
-    {
-      urutan: "008",
-      name: "Tia Mutiara",
-      loket: "7",
-    },
-    {
-      urutan: "007",
-      name: "David",
-      loket: "8",
-    },
-  ]);
+  const [data, setData] = useState([]);
   const [calling, setCalling] = useState(false);
+
+  const fetchLayanan = async () => {
+     
+    const res = await axios.get(
+      `${process.env.REACT_APP_API_URL}/antrian-display`
+    );
+    console.log(res.data);
+
+    setData(res.data);
+   
+  };
+  
 
   useEffect(() => {
     if (calling) {
@@ -53,6 +25,8 @@ function LihatAntrian() {
         setCalling(false);
       }, 5000);
     }
+    fetchLayanan();
+
   }, [calling]);
 
   if (calling) {
